5f466e9c87fecb24dce2e7c79567f1b6ac101ff9
lin <465382251@qq.com>
2020-10-10 14:12:48 +0800

    add websocket

---
+++ src/Api/WebSocket/Socket.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author lin <465382251@qq.com>
+ * */
+
+namespace Lin\Okex\Api\WebSocket;
+
+
+class Socket
+{
+
+}
---
+++ src/OkexWebSocket.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author lin <465382251@qq.com>
+ * */
+
+namespace Lin\Okex;
+
+
+class OkexWebSocket
+{
+    public function config(array $init=[]){
+
+    }
+
+    public function subscribe(array $subscribe,$callback){
+
+    }
+
+    public function unsubscribe(array $subscribe,$callback){
+
+    }
+}
