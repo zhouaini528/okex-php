a939148b4a435ca738bfd4239da480713c006cb5
lin <465382251@qq.com>
2020-10-23 11:37:04 +0800

    Get partial private channel subscription data

--- src/OkexWebSocket.php
+++ src/OkexWebSocket.php
@@ -15,39 +15,71 @@ class OkexWebSocket
     private $client=null;
     private $config=[];
 
+    /**
+     * @param array $config
+     */
     public function config(array $config=[]){
         $this->config=$config;
     }
 
+    /**
+     * @return SocketServer
+     */
     public function server(){
         return $this->server = new SocketServer($this->config);
     }
 
+    /**
+     * @return SocketClient|null
+     */
     public function client(){
         if($this->client!==null) return $this->client;
         return $this->client = new SocketClient($this->config);
     }
 
+    /**
+     *
+     */
     public function start(){
         $this->server()->start();
     }
 
+    /**
+     * @param array $keysecret
+     */
     function keysecret(array $keysecret=[]){
         $this->client()->keysecret($keysecret);
     }
 
+    /**
+     * @param array $sub
+     */
     public function subscribe(array $sub=[]){
         $this->client()->subscribe($sub);
     }
 
+    /**
+     * @param array $sub
+     */
     public function unsubscribe(array $sub=[]){
         $this->client()->unsubscribe($sub);
     }
 
+    /**
+     * @param array $sub
+     * @param null $callback
+     * @param bool $daemon
+     * @return array
+     */
     public function getSubscribe(array $sub,$callback=null,$daemon=false){
         return $this->client()->getSubscribe($sub,$callback,$daemon);
     }
 
+    /**
+     * @param null $callback
+     * @param bool $daemon
+     * @return array
+     */
     public function getSubscribes($callback=null,$daemon=false){
         return $this->client()->getSubscribes($callback,$daemon);
     }
